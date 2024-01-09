4a0e1765da2334bf4c41aa40fcfd751a43d1c834
yuemengrui <1401190335@qq.com>
2024-01-09 16:57:11 +0800

    add healthcheck

--- Embedding_Server/info/modules/__init__.py
+++ Embedding_Server/info/modules/__init__.py
@@ -1,8 +1,9 @@
 # *_*coding:utf-8 *_*
 from fastapi import FastAPI
-from . import embedding, rerank
+from . import embedding, rerank, health
 
 
 def register_router(app: FastAPI):
     app.include_router(router=embedding.router, prefix="", tags=['Embedding'])
     app.include_router(router=rerank.router, prefix="", tags=['reRank'])
+    app.include_router(router=health.router, prefix="", tags=['health'])
