1a07440ee5e66fdfcc21e27f28080deeb1daf2cd
yuemengrui <1401190335@qq.com>
2024-07-23 13:36:00 +0800

    update

--- Embedding_Server/info/modules/health.py
+++ Embedding_Server/info/modules/health.py
@@ -7,7 +7,7 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
-@router.api_route(path='/ai/health', methods=['GET'], summary="health")
+@router.api_route(path='/ai/health', methods=['GET'], summary="health", include_in_schema=False)
 @limiter.limit("120/minute")
 async def health(request: Request):
     return JSONResponse({'msg': 'I am very healthy'})
